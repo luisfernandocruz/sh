0a1706ed3b00bc3fe0ed16d84dd437b444991b6c
SKIPPER-PC\Luis Fernando Cruz <fergorritas@gmail.com>
2017-10-23 14:04:24 -0500

    Creacion del Auth---

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::resource('detalle', 'DetalleController');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
